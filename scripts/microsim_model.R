34f2fa653fe200efe1926ef5e84f2cfe8849a0ac
usr110 <m.aliabbas@gmail.com>
2023-09-28 11:58:38 +1000

    Add more comments

--- scripts/microsim_model.R
+++ scripts/microsim_model.R
@@ -51,9 +51,9 @@ back_hdata <-  back_hdata |>
 # Combine baseline demographics and exposure data with background health data
 synth_pop <- left_join(synth_pop, back_hdata)
 
+# Function to return 
 prob_age_sex <- function(data, hdata, colname = "sick_prob_allc", seed = 1, cycle = 1) {
   set.seed(seed)
-  res <- list()
   # Get age and sex specific sick_prob from background death rate
   # The cycle is the current year and is added to the baseline's age of an individuals to get the current age
   # So age = baseline_age + cycle - 1 - as cycle starts with 1 hence subtracting 1 from it
@@ -67,25 +67,40 @@ prob_age_sex <- function(data, hdata, colname = "sick_prob_allc", seed = 1, cycl
   return(nsick_prob)
 }
 
+
+# Get the state for each individual by looking at the transition probability
+# If transition probability is less than sick_prob, then it happens, otherwise the agent (or the individual)
+# remain in the same state
 get_state <- function(rd, cycle = 1, cause = "allc", cm) {
+  # Get unique index for the agent
   vi <- rd["rowname"] |> as.numeric()
+  # Get previous state of the agent
   prev_state <-  cm[vi, 1] |> as.character()
+  # Get current state of the agent
   curr_state <-  cm[vi, 2] |> as.character()
+  # Create cause specific state name
   curr_cause <- paste0('S_', toupper(cause))
+  # If the agent has already died, then they remain in the same 'dead' state
   if (prev_state == 'D' ||
       (!is.na(curr_state) && curr_state == 'D') ||
       rd[paste0("prob_allc_", cycle)] |> as.numeric() == 0)
     return('D')
   else{
     # prob <- rbernoulli(1, p = rd[paste0("prob_", cause, "_", cycle)] |> as.numeric())
+    # If however the uniform random probablity is greater than age and sex specific sick_prob, then transition to the
+    # specific cause (curr_cause) takes place
     prob <- runif(1)
     if (prob < rd[paste0("prob_", cause, "_", cycle)] |> as.numeric()){
+      # All-cause mortality is a special cause that takes an agent to the 'dead' cause
       if (cause == 'allc')
         return ('D')
       else{
+        # If the cause is not all-cause, use curr_cause as the new state
         if(is.na(curr_state))
           return (curr_cause)
         else{
+          # If the curr_cause already exists in the history of the agent, nothing happens as the agent already has that specific cause
+          # In the else section, we make sure that if an agent catches a specific disease, we explicitly remove the healthy state from it
           if (grepl(curr_cause, curr_state, ignore.case = F))
             return(curr_state)
           else
@@ -94,6 +109,7 @@ get_state <- function(rd, cycle = 1, cause = "allc", cm) {
         }
       }
     }else{
+      # The agent remains in the previous state if the transition probablity was greater than random uniform sample of 1
       return(ifelse(is.na(curr_state), prev_state, curr_state))
     }
   }
@@ -102,17 +118,22 @@ get_state <- function(rd, cycle = 1, cause = "allc", cm) {
 # Create a df for 
 synth_pop_wprob <- synth_pop|> rownames_to_column()
 # 
-# # Matrix to save current states
+# Matrix to save current states
+# with dimensions: (rows: number of individuals, cols: number of classes (or years) + 1 (for the 0th year))
 m <- matrix(nrow = n.i, ncol = n.c + 1,
             dimnames = list(paste0("id", 1:n.i, sep = ""),
                             paste0("c", 0:n.c, sep = "")))
 
 # The default state is healthy for everyone - before simulation starts
 m[,1] <- v.M_1
+# Read the names of all columns starting with the 'sick' word, so that list of causes (or diseases) can be generated
 diseases <- sapply(strsplit(back_hdata |> dplyr::select(starts_with("sick")) |> names(), "\\_"), "[", 3)
+# Select only a handful of cause for now, which are: all-cause-mortality, ischaemic heart disease", stroke and lung Cancer
 diseases <- c("allc", "ishd", "tbalc", "strk")
+# Assign an easily understandable label to each of the causes/diseases
 lbls <- c("Dead", "Healthy", "All-cause mortality", "Ischaemic Heart Disease", "Stroke", "Lung Cancer")
 
+# Record the time it takes to run the piece of code with tic()
 require(tictoc)
 tic()
 stop <- F
@@ -138,6 +159,9 @@ for (i in 1:n.c){
        
   }
 }
+# Stop recording of time spent running the code
+toc()
+
 
 l <- data.frame(Var1 = c('H', 'D', paste0('S_', toupper(diseases))), freq = 0, c = 0)
 for (ind in 1:n.c){
@@ -153,11 +177,10 @@ for (ind in 1:n.c){
 }
 
 l[l$c == 0 & l$Var1 == 'H',]$freq <- 100
-toc()
 
 l$c <- as.factor(l$c)
 
-l <- l |> filter(!is.na(Var1)
+l <- l |> filter(!is.na(Var1))
 
 ggplot(l) +
   aes(x = c, y = freq, fill = Var1) +
