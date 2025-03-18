b4012c2be98289f3487f08edfa9d23e7833ba900
Ali Abbas <m.aliabbas@gmail.com>
2025-03-18 14:02:59 +0000

    Use rate and then convert them back to prob. Also add prevalance dataset

--- scripts/microsim_model.R
+++ scripts/microsim_model.R
@@ -26,53 +26,19 @@ sample_size <- 1000
 
 ## Synthetic population file with exposures and physical activity
 ## Belen (19/02/2025): for the paper we might want to start froom the synth pop with PA and exposures and assign RRs.
-synth_pop <- read_csv(here("manchester/simulationResults/ForPaper/1_reference/health/04_exposure_and_rr/pp_exposure_2021.csv"))
+synth_pop <- read_csv(here("jibe health/base_pp_exposure_RR_2021.csv"))
 
 ## Health transitions
 
-hd <- read_csv(here("manchester/health/processed/health_transitions_manchester_prevalence.csv")) 
+hd <- read_csv(here("jibe health/health_transitions_manchester.csv")) 
 
 
 if (sample_size > 0)
   synth_pop <- sample_n(synth_pop, sample_size)
 
+synth_pop <- synth_pop |> rename(sex = gender)
 
-
-## Clean column names
-# Remove individual causes that have a combined effect
-synth_pop <- synth_pop |> dplyr::select(-contains(c("all_cause", "IHD", "lung_cancer", "stroke", "T2D", "CVD")), starts_with("RR_cyc"))
-
-# Remove prefix for AP, PA and combined RRs
-synth_pop <- synth_pop |> rename_all(~ stringr::str_replace(., regex("^RR_ap_cyc_|^RR_pa_cyc_|^RR_cyc_", ignore_case = TRUE), ""))
-
-synth_pop <- synth_pop |> rename(all_cause_mortality = all_cause, 
-                                  diabetes = T2D,
-                                  all_cause_dementia = total_dementia,
-                                  coronary_heart_disease = IHD,
-                                  endometrial_cancer = endo_cancer,
-                                  sex = gender) |> janitor::clean_names()
-# sort(names(synth_pop))
-# [1] "age"              "all_cause"        "breast_cancer"    "colon_cancer"     "COPD"             "CVD"              "endo_cancer"     
-# [8] "gender"           "head_neck_cancer" "id"               "IHD"              "liver_cancer"     "LRI"              "lsoa21cd"        
-# [15] "lung_cancer"      "myeloid_leukemia" "myeloma"          "Parkinson"        "respiratory"      "stomach_cancer"   "stroke"          
-# [22] "T2D"              "total_cancer"     "total_dementia"  
-
- 
-
-hd <- read_csv("D:/Users/aa797/manchester/input/health/health_transitions_manchester.csv") 
-
-
-# vigorous_mmet = 3
-# 
-# # Read manchester specific synth_pop filename
-# synth_pop <- synth_pop |> 
-#   ungroup() |> 
-#   dplyr::select(id, age, gender, mmetHr_cycle, mmetHr_walk) |> 
-#   rename (sex = gender) %>%
-#   # mutate(total_tr_pa = mmetHr_cycle + mmetHr_walk, total_non_tr_pa = otherSport_wkhr * vigorous_mmet,
-#   #                                 total_mmet = total_tr_pa+total_non_tr_pa) 
-#   {if (sample_size > 0) sample_n(., sample_size) else .}
-
+names(synth_pop) <- gsub("(RR_|base_)", "", names(synth_pop))
 
 # Number of individuals
 n.i <- synth_pop |> nrow()
@@ -85,37 +51,41 @@ v.M_1 <- rep("healthy", n.i)
 
 # Function to return 
 
-get_state <- function(rd, cycle = 1, cause = "allc", cm) {
+get_state <- function(rd, cycle = 1, cause = "allc", cm, ind_spec_rate) {
   
-  
+  # browser()
   # Get unique index for the agent
   vi <- rd["rowname"] |> as.numeric()
   
-  # browser()
   # Get previous state of the agent
-  prev_state <-  cm[vi, 1] |> as.character()
+  prev_state <-  cm[1] |> as.character()
   # Get current state of the agent
-  curr_state <-  cm[vi, 2] |> as.character()
+  curr_state <-  cm[2] |> as.character()
   # Create cause specific state name
   # curr_cause <- paste0('S_', toupper(cause))
   curr_cause <- cause
   # Get age, sex and disease specific prob
-  dis_prob <- rd[cause] |> as.numeric()
+  dis_rate <- rd[cause] |> as.numeric()
+  
+  if (length(ind_spec_rate) == 0)
+    ind_spec_rate <- 0
+  
+  dis_rate <- dis_rate * ind_spec_rate
+  
+  dis_prob <- 1 - exp(-dis_rate)
+  
     # Get age, sex and all-cause-mortality prob
   all_cause_prob <- rd["all_cause_mortality"] |> as.numeric()
   
-  # print(vi)
-  # print(cause)
-  
   # If the agent has already died, then they remain in the same 'dead' state
-  if (prev_state == 'dead' || (!is.na(curr_state) && curr_state == 'dead') || (all_cause_prob == 0))
+  if ((!is.na(curr_state) && prev_state == 'dead') || (!is.na(curr_state) && curr_state == 'dead') || (all_cause_prob == 0))
     return('dead')
   else{
-    # prob <- rbernoulli(1, p = rd[paste0("prob_", cause, "_", cycle)] |> as.numeric())
-    # If however the uniform random probablity is greater (ALi, is this lower?) than age and sex specific sick_prob, then transition to the
+    # If however the uniform random probability is lower than age and sex specific sick_prob, then transition to the
     # specific cause (curr_cause) takes place
     prob <- runif(1)
-    if (!is.na(dis_prob) && prob < dis_prob){
+    # if (!is.na(dis_prob) & prob < dis_prob){
+    if (!any(is.na(dis_prob)) && all(prob < dis_prob)) {
       # All-cause mortality is a special cause that takes an agent to the 'dead' state
       if (cause == 'all_cause_mortality')
         return ('dead')
@@ -141,48 +111,53 @@ get_state <- function(rd, cycle = 1, cause = "allc", cm) {
 }
 
 
-multiply_similar_suffix_columns <- function(df, un = "exp_") {
-  # Get all column names
-  col_names <- names(df)
+# Function to multiply columns with a given suffix and remove original columns
+multiply_columns_with_suffix <- function(df, suffix) {
+  # Find columns that end with the given suffix
+  columns_to_multiply <- names(df)[str_detect(names(df), paste0("_", suffix, "$"))]
   
-  # Function to extract the common part of column names
-  extract_common <- function(name) {
-    parts <- strsplit(name, un)[[1]]
-    length_parts <- length(parts)
-    parts[length_parts]
-  }
-  
-  # Get unique common parts
-  common_parts <- unique(sapply(col_names, extract_common))
-  
-  # For each common part, multiply corresponding columns
-  for (part in common_parts) {
-    matching_cols <- col_names[sapply(col_names, function(x) grepl(part, x))]
+  # If there are columns to multiply
+  if (length(columns_to_multiply) > 0) {
+    # Create a new column name
+    new_column_name <- paste0("all_path_", suffix)
     
-    if (length(matching_cols) > 1) {
-      new_col_name <- paste0("RR_", part)
-      df[[new_col_name]] <- Reduce(`*`, df[matching_cols])
-    }
+    # Multiply the columns
+    df[[new_column_name]] <- apply(df[columns_to_multiply], 1, prod)
+    
+    # Remove original columns
+    df <- df %>% dplyr::select(-all_of(columns_to_multiply))
   }
   
   return(df)
 }
 
+# Function to process all suffixes and remove original columns after multiplication
+process_all_suffixes <- function(df, cause_df) {
+  # Extract unique suffixes from the 'cause' column of the cause data frame
+  suffixes <- unique(cause_df$cause)
+  
+  # Apply multiplication for each suffix and remove original columns
+  for (suffix in suffixes) {
+    df <- multiply_columns_with_suffix(df, suffix)
+  }
+  
+  return(df)
+}
 
-synth_pop <- synth_pop %>% rename_with(~ paste0("exp_", .x), -c("id","age", "sex", "lsoa21cd")) 
+synth_pop_wprob <- synth_pop
 
-# Create a df for 
-synth_pop_wprob <- synth_pop |> 
-  rownames_to_column() |> 
-  left_join(hd |> 
-              pivot_wider(id_cols = c(age, sex, lsoa21cd), 
-                          names_from = cause, values_from = prob), 
-            join_by(age, sex, lsoa21cd))
+synth_pop_wprob <- process_all_suffixes(synth_pop_wprob, 
+                                        hd |> dplyr::select(cause) |> distinct()) 
 
-# Replace NAs with 1
-#synth_pop_wprob[is.na(synth_pop_wprob)] <- 1 
+names(synth_pop_wprob) <- str_replace(names(synth_pop_wprob), "^all_path_|ap_|pa_", "")
 
-synth_pop_wprob <- multiply_similar_suffix_columns(synth_pop_wprob |> dplyr::select(-c("id","age", "sex", "lsoa21cd"))) 
+synth_pop_wprob <- synth_pop_wprob |> tibble::rowid_to_column("rowname")
+
+synth_pop_wprob <- synth_pop_wprob |> 
+  rename(
+    "parkinson" = "parkinson's_disease",
+    "copd" = "COPD"
+  ) 
 
 # 
 # Matrix to save current states
@@ -191,42 +166,54 @@ m <- matrix(nrow = n.i, ncol = n.c + 1,
             dimnames = list(paste0("id", 1:n.i, sep = ""),
                             paste0("c", 0:n.c, sep = "")))
 
+
+# Read prevalence dataset
+prev <- read_csv("jibe health/prevalence_id.csv")
+
+td <- synth_pop_wprob |> 
+  left_join(prev) |> 
+  dplyr::select(id, diseases) |> 
+  mutate(diseases = case_when(is.na(diseases) ~ "healthy", 
+                              TRUE ~ diseases)) |> 
+  arrange(id)
+
 # The default state is healthy for everyone - before simulation starts
-m[,1] <- v.M_1
+m[,1] <- td$diseases
+
+#rownames(m) <- td$id
 # Read the names of all columns starting with the 'sick' word, so that list of causes (or diseases) can be generated
 diseases <- unique(hd$cause)
-
-synth_pop_wprob <- synth_pop_wprob |> dplyr::select(c("rowname", contains("RR")))
-
-names(synth_pop_wprob) <- sub("RR_", "", names(synth_pop_wprob), fixed = TRUE)
-
-# sm_synth_pop_wprob <- synth_pop_wprob[1:1000,]
-# sm_m <- m[1:1000,] |> as.matrix()
 
 # 
 # Record the time it takes to run the piece of code with tic()
 require(tictoc)
 tic()
 stop <- F
+
 for (incyc in 1:n.c){
   for (dis in diseases){
-    cstate <- list()
-    # dis <- diseases[1]
-    # for (index in 1:nrow(synth_pop_wprob)){
-    #   index <- 1
-    #   incyc <- 1
-    #       cstate[index] <- get_state(rd = synth_pop_wprob[index,], cycle = incyc,
-    #                                  cause = dis, cm = m[, c(paste0("c", incyc - 1), paste0("c", incyc))])
-    # 
-    # }
-    # 
-    # print(table(unlist(cstate)))
-    cstate <- future_apply(synth_pop_wprob, 1, get_state,
-                           cycle = incyc, cause = dis,
-                           cm = m[, c(paste0("c", incyc - 1), paste0("c", incyc))], future.seed = T)
-
+    cstate <- future_apply(synth_pop_wprob, 1, function(row) {
+      if (dis == "all_cause_mortality"){
+        filtered_rate <- hd[hd$age == row["age"] |> as.numeric() + incyc - 1 &
+                            hd$sex == row["sex"] |> as.numeric() &
+                            hd$cause == dis &
+                            hd$location_code == row["lsoa21cd"] |> as.character() &
+                            hd$measure == "deaths", ] |>
+          dplyr::select(rate) |> pull()
+        }else{
+          filtered_rate <- hd[hd$age == row["age"] |> as.numeric() + incyc - 1 &
+                              hd$sex == row["sex"] |> as.numeric() &
+                              hd$cause == dis &
+                              hd$location_code == row["ladcd"] |> as.character() &
+                              hd$measure == "incidence", ] |>
+            dplyr::select(rate) |> pull()
+        }
+      get_state(row, cycle = incyc, cause = dis,
+                cm = m[row["rowname"] |> as.numeric(),
+                       c(paste0("c", incyc - 1), paste0("c", incyc))],
+                ind_spec_rate = filtered_rate)
+    }, future.seed = TRUE)
     m[, incyc + 1] <- cstate
-
   }
 }
 
@@ -241,7 +228,7 @@ for (ind in 1:n.c){
   names(df) <- 'states'
   tbl <- df |> 
     group_by(states) |> 
-    summarise(cn = n()) |> 
+    summarise(cn = dplyr::n()) |> 
     mutate(freq = round(cn / sum(cn) * 100, 1), c = ind) |> 
     dplyr::select(-cn)
   l <- plyr::rbind.fill(l, tbl)
@@ -254,7 +241,7 @@ l$c <- as.factor(l$c)
 l <- l |> filter(!is.na(states))
 
 # Generate historic state transitions of all diseases + dead
-ggplot(l) +
+ggplot(l |> filter(freq > 0)) +
   aes(x = c, y = freq, fill = states) +
   geom_col() +
   labs(x = "Years", y = "Frequency (%)", title = "State transitions over the years") +
@@ -280,4 +267,4 @@ m |> as.data.frame() |>
 # ggsave(paste0("diagrams/state_trans-n.c-",n.c, "-n.i-", n.i, "-n.d-", length(diseases), ".png"), height = 5, width = 10, units = "in", dpi = 600, scale = 1)
 # # 
 # # # Also save state transitions as a CSV
-arrow::write_dataset(m |> as.data.frame(), paste0("data/state_trans-n.c-",n.c, "-n.i-", n.i, "-n.d-", length(diseases), ".parquet"))
+# arrow::write_dataset(m |> as.data.frame(), paste0("data/state_trans-n.c-",n.c, "-n.i-", n.i, "-n.d-", length(diseases), ".parquet"))
