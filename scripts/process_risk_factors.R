57c01f439e6d9165c7f465a29440d3ec33484252
Ali Abbas <m.aliabbas@gmail.com>
2025-03-19 12:51:35 +0000

    Add script to process relative risk prepared by Belen

---
+++ scripts/process_risk_factors.R
@@ -0,0 +1,26 @@
+# Replace dash with underscore
+# Add missing causes
+# Rename risk disease
+disease_risks <- read_csv("jibe health/health/disease risks.csv")
+
+# Replace dash in outcome with underscore
+disease_risks$outcome <- gsub("-", "_", disease_risks$outcome)
+
+# Rename parkinsons disease to parkinson
+disease_risks[disease_risks$risk_factor == "parkinsons_disease",]$risk_factor <- "parkinson"
+
+# # Add a new row for myeloid_leukemia that has cancer risks
+# disease_risks <- rbind(disease_risks,
+#       disease_risks |>  
+#         filter(risk_factor == "cancer") |> 
+#         mutate(risk_factor = "myeloid_leukemia"))
+# 
+# # Add a new row for myeloma that has cancer risks
+# disease_risks <- rbind(disease_risks,
+#                        disease_risks |>  
+#                          filter(risk_factor == "cancer") |> 
+#                          mutate(risk_factor = "myeloma"))
+
+
+# Write this to health folder as mod_disease_risks
+write_csv(disease_risks, "jibe health/mod_disease_risks.csv")
