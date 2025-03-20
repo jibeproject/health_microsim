3b94de9bf41cef52079262d9b5658a68a9efffc2
Ali Abbas <m.aliabbas@gmail.com>
2025-03-20 17:34:21 +0000

    Add pm2.5 dose response function for all_cause_mortality, copd, ihd, lung cancer and stroke

---
+++ scripts/pm2.5_dose_response.R
@@ -0,0 +1,60 @@
+#' Calculate relative risk given NO2 exposure level
+#'
+#' Calculate the relative risk (RR) for each person in the baseline population for each
+#' disease related to air pollution and each scenario based on the individual NO2 exposure levels
+#'
+#' This function performs the following steps:
+#'
+#' \itemize{
+#' \item various checks to ensure the correct NO2 exposure levels (doses) and disease are read in
+#'
+#' \item get the lookup table for the required dose response functions which contains for each dose
+#'   the median RR values
+#'
+#' \item for the NO2 exposure doses in the baseline population find the needed RR by extrapolating the
+#'   dose responses given in the lookup table
+#'
+#' }
+#'
+#' @param cause name of disease
+#' @param dose vector of NO2 exposure levels from individuals for a given age range and scenario
+#'
+#' @return data frame of relative risks for each person in the baseline population and each NO2 related disease in each scenario
+#'
+#' @export
+
+
+pm2.5_dose_response <- function(cause, dose) {
+  # Check there are NAs in dose or the classes is not numeric
+  if (sum(is.na(dose)) > 0 || class(dose) != "numeric") {
+    stop("Please provide dose in numeric")
+  }
+  # check that the correct disease name is used
+  if (!cause %in% c(
+    "all_cause_pm",
+    "copd_pm",
+    "ihd_pm",
+    "lc_pm",
+    "stroke_pm"
+  )) {
+    print("Unsupported cause/disease: ", cause)
+    stop("Please select from \n
+         all_cause_pm, copd_pm, ihd_pm , lc_pm, stroke_pm")
+  } # End if unsupported causes
+  
+  # read in dose response functions for disease cause
+  # contains relative risk with upper and lower limits for a variety of doses
+  lookup_table <- get(cause)
+  names(lookup_table) <- base::tolower(names(lookup_table))
+  lookup_df <- setDT(lookup_table)
+  
+  # interpolate the values in the lookup table to get RR values for all NO2 exposure doses
+  # in the baseline population
+  suppressWarnings(
+    rr <- approx(
+      x = lookup_df$dose, y = lookup_df$rr,
+      xout = dose, yleft = 1, yright = min(lookup_df$rr)
+    )$y
+  )
+  return(data.frame(rr = rr))
+}
