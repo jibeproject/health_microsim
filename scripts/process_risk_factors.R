f0bd997301a9c387cd8c0fb39358560bfc34932a
Ali Abbas <m.aliabbas@gmail.com>
2025-03-21 13:15:20 +0000

    Typo

--- scripts/process_risk_factors.R
+++ scripts/process_risk_factors.R
@@ -6,7 +6,7 @@ disease_risks <- read_csv("jibe health/health/disease risks.csv")
 # Replace dash in outcome with underscore
 disease_risks$outcome <- gsub("-", "_", disease_risks$outcome)
 
-# Rename parkinsons disease to parkinson
+# Rename parkinsons_disease to parkinson
 disease_risks[disease_risks$risk_factor == "parkinsons_disease",]$risk_factor <- "parkinson"
 
 # # Add a new row for myeloid_leukemia that has cancer risks
