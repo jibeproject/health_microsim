f0977eccc6604d70d8d518ccaee5fb19e66371b4
Ali Abbas <m.aliabbas@gmail.com>
2025-06-30 14:35:45 +0100

    Add scrip to calculate averted deaths

--- scripts/deaths_summary.R
+++ scripts/deaths_summary.R
@@ -3,7 +3,8 @@ calculate_deaths <- function(dataset, scen_name){
   base_pop <- dataset |> filter(value != "dead", cycle == 1) |> group_by(cycle) |> summarise(n_distinct(id)) |>  pull()
   deaths <- dataset |> filter(value == "dead") |> group_by(cycle) |> summarise(deaths = n_distinct(id)) |> mutate(new_deaths = deaths - lag(deaths, 1))
   deaths$new_deaths <- ifelse(is.na(deaths$new_deaths), deaths$deaths, deaths$new_deaths)
-  return(deaths |> mutate(unit_death = new_deaths/base_pop,
+  return(deaths |> mutate(base_pop = base_pop,
+                          unit_death = new_deaths/base_pop,
                           total_deaths = round(unit_death * 2827285), 
                           name = scen_name))
   
@@ -11,5 +12,7 @@ calculate_deaths <- function(dataset, scen_name){
 }
 
 deaths <- rbind(calculate_deaths(all_data$base, "reference"), calculate_deaths(all_data$green, "green"),
-                calculate_deaths(all_data$safeStreet, "safeStreet"), calculate_deaths(all_data$both, "both"))
+                calculate_deaths(all_data$safeStreet, "safeStreet"), 
+                calculate_deaths(all_data$both, "both"))
 
+deaths |> group_by(name) |> summarise(total_deaths = sum(total_deaths)) |> mutate(averted_deaths = total_deaths[name == "reference"] - total_deaths) |> print()
